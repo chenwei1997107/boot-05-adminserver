be5b12dd4d87a9632db70a8203bc238addf79657
CW <1353647867@qq.com>
2022-08-10 19:36:44 +0800

    hot-fix second commit

--- src/main/java/com/atguigui/adminserver/Boot05AdminserverApplication.java
+++ src/main/java/com/atguigui/adminserver/Boot05AdminserverApplication.java
@@ -11,6 +11,7 @@ public class Boot05AdminserverApplication {
     public static void main(String[] args) {
         SpringApplication.run(Boot05AdminserverApplication.class, args);
         System.out.println("1111111111");
+        System.out.println("hot-fix");
     }
 
 }
